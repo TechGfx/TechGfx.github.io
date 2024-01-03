5e59b3d4a22e5b4d824891854146b1044648f857
TechGfx <technet7992@gmail.com>
2024-01-03 15:47:26 -0500

    lvdesign12345.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
-
-import vercel from '@astrojs/vercel/serverless';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vercel()],
-  output: 'server',
-  adapter: vercel(),
+  integrations: [tailwind()],
+  output: "server",
+  adapter: vercel()
 });
