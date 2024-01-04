e77b25bbe1b3ac8dcb5cd9341231153c3220d346
TechGfx <technet7992@gmail.com>
2024-01-03 19:14:37 -0500

    lp101

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://techgfx.github.io',
-  base: '',
+  base: '.',
   integrations: [tailwind()]
 });
